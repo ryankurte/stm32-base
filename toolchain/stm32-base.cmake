267834d2ad67831e9dd9a5ee90ce81d2e003a5da
Ryan Kurte <ryankurte@gmail.com>
2016-03-25 17:51:30 +1300

    Added linker file

--- toolchain/stm32-base.cmake
+++ toolchain/stm32-base.cmake
@@ -8,34 +8,58 @@ endif(NOT DEFINED DEVICE)
 message("Device: ${DEVICE}")
 string(TOUPPER ${DEVICE} DEVICE_U)
 
-# Determine short device type
-
 # Determine device family
 string(REGEX MATCH "^(STM32F[0-9])" CPU_FAMILY_U "${DEVICE_U}")
 string(TOLOWER ${CPU_FAMILY_U} CPU_FAMILY_L)
 message("Family: ${CPU_FAMILY_U}")
 
+# Determine short device type
 string(REGEX MATCH "^(STM32F[0-9][0-9][0-9])" CPU_TYPE_U "${DEVICE_U}")
 string(TOLOWER ${CPU_TYPE_U} CPU_TYPE_L)
 message("Type: ${CPU_TYPE_U}")
 
+# Check for required processor info
+# TODO: wonder if I could parse (most of) this from the config files
+if(NOT DEFINED FLASH_START)
+set(FLASH_START 0x00000000)
+message("No FLASH_START defined. Using default: ${FLASH_START}")
+endif(NOT DEFINED FLASH_START)
 
-#if(NOT DEFINED FLASH_START)
-#set(FLASH_START 0x00000000)
-#message("No FLASH_START defined. Using default: ${FLASH_START}")
-#endif(NOT DEFINED FLASH_START)
+if(NOT DEFINED FLASH_LENGTH)
+set(FLASH_LENGTH 1024k)
+message("No FLASH_LENGTH defined. Using default: ${FLASH_LENGTH}")
+endif(NOT DEFINED FLASH_LENGTH)
 
+if(NOT DEFINED RAM_LENGTH)
+set(RAM_LENGTH 128k)
+message("No RAM_LENGTH defined. Using default: ${RAM_LENGTH}")
+endif(NOT DEFINED RAM_LENGTH)
+
+if(NOT DEFINED CCRAM_LENGTH)
+set(CCRAM_LENGTH 64k)
+message("No CCRAM_LENGTH defined. Using default: ${CCRAM_LENGTH}")
+endif(NOT DEFINED CCRAM_LENGTH)
+
+# Set CPU type for compiler
 set(CPU_TYPE "m4")
 
 # Include libraries
 include(${CMAKE_CURRENT_LIST_DIR}/../drivers/CMSIS/cmsis.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../drivers/STM32F4xx_HAL_Driver/hal.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/../drivers/BSP/bsp.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/../middlewares/middlewares.cmake)
 
+# Generate linker script
+set(LINKER_SCRIPT stm32f4.ld)
+configure_file(${CMAKE_CURRENT_LIST_DIR}/../stm32f4.ld.in
+	${PROJECT_BINARY_DIR}/${LINKER_SCRIPT})
 
 # Set compiler flags
 # Common arguments
-set(COMMON_DEFINITIONS "-Wextra -Wall -Wno-unused-parameter -mcpu=cortex-${CPU_TYPE} -mthumb -fno-builtin -ffunction-sections -fdata-sections -fomit-frame-pointer -D${DEVICE} ${OPTIONAL_DEBUG_SYMBOLS}")
+add_definitions("-Wextra -Wall -Wno-unused-parameter")
+add_definitions("-mcpu=cortex-${CPU_TYPE} -mthumb")
+add_definitions("-fno-builtin -ffunction-sections -fdata-sections -fomit-frame-pointer")
+add_definitions("-D${DEVICE} -D${CPU_TYPE_U}xx -D${CPU_FAMILY_U} ${OPTIONAL_DEBUG_SYMBOLS}")
 set(DEPFLAGS "-MMD -MP")
 
 # Enable FLTO optimization if required
