b2394ac4185d16f05a20b51187b826acd4e383d2
Ryan <ryankurte@users.noreply.github.com>
2018-06-15 11:42:53 +1200

    Update arm-gcc.cmake

--- toolchain/arm-gcc.cmake
+++ toolchain/arm-gcc.cmake
@@ -10,7 +10,6 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 # Override compiler
-set(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
